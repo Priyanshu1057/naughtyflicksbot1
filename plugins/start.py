a914196f98dc415312927182b5b78fd1e15c9792
Flix_botz <99806982+Priyanshu1057@users.noreply.github.com>
2025-10-27 17:24:49 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -101,13 +101,6 @@ async def start_command(client: Client, message: Message):
                     protect_content=False,
                     quote=True
                 )
-
-    if not verify_status['is_verified'] and not is_premium:
-            sticker_msg = await message.reply_sticker("CAACAgUAAxkBAAEPAAHbaIDNXHGkfiVuQ8GpQn_ObVULoXsAAgQAA8EkMTGJ5R1uC7PIEDYE")  # Replace with your sticker ID
-        await asyncio.sleep(1)
-        await sticker_msg.delete()
-
-
     
     # ✅ Check Force Subscription
     if not await is_subscribed(client, user_id):
